fb4ea642f010e623a8c4839bde90b526d2abe607
Thiago Durante Pires <thdurante@gmail.com>
2015-12-05 17:40:34 -0200

    Criação da estrutura inicial do módulo iOS

---
+++ ios/Fit Life/Fit Life/viewcontrollers/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Fit Life
+//
+//  Created by Thiago Durante Pires on 05/12/15.
+//  Copyright © 2015 CodingLab. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios/Fit Life/Fit LifeTests/Fit_LifeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Fit_LifeTests.swift
+//  Fit LifeTests
+//
+//  Created by Thiago Durante Pires on 05/12/15.
+//  Copyright © 2015 CodingLab. All rights reserved.
+//
+
+import XCTest
+@testable import Fit_Life
+
+class Fit_LifeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
