a8d1f79bf2ade63c64394bd531c47878c136f368
Gabriel <gabrielrascovit@inf.ufg.br>
2015-12-07 01:05:49 -0200

    Criação do AlunoService que contém o método login; método save() do scaffold de Aluno sobrescrito para encode da senha como SHA1 (temporário até não criarmos nossas views)

--- web/grails-app/controllers/br/com/codinglab/AlunoController.groovy
+++ web/grails-app/controllers/br/com/codinglab/AlunoController.groovy
@@ -1,7 +1,26 @@
 package br.com.codinglab
 
+import grails.converters.JSON
+
 class AlunoController {
 
     static scaffold = true
+    AlunoService alunoService
+
+    /* Sobrescreve o método save() do scaffold para passar a senha como SHA1 */
+    def save() {
+        params.senha = params.senha.encodeAsSHA1()
+        def aluno = new Aluno(params)
+        if (!aluno.save(flush: true)) {
+            render(view: "create", model: [aluno: aluno])
+            return
+        }
+        flash.message = message(code: 'default.created.message', args: [message(code: 'aluno.label', default: 'Aluno'), aluno.id])
+        redirect(action: "show", id: aluno.id)
+    }
+
+    def login() {
+        render alunoService.login(params.cpf, params.senha)
+    }
 
 }
